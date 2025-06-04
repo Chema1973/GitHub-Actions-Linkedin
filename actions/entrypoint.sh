49ac0d8e9621725d198df5dc57b21a526b9a8cb8
Chema1973 <chema.pairet@gmail.com>
2025-06-04 09:01:42 +0200

    Nuevos archivos

---
+++ actions/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+api_url="https://swapi.dev/api/people/${INPUT_PERSON_ID}"
+echo $api_url
+
+person_name=$(curl "${api_url}" | jq ".name")
+echo $person_name
+
+echo "::set-output name=person_name::$person_name"
